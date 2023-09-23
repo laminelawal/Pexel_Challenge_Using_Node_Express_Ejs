ac4cb3ed9b873ab7f634e206b41dfc6d81248f4f
laminelawal <macbookpro@MBP-di-macbook.fritz.box>
2023-09-23 12:41:13 +0200

    Update --v0.3

--- index.js
+++ index.js
@@ -1,114 +1,17 @@
-// // IMPORTING FILES
 import express from "express"
 import bodyParser from "body-parser"
 import axios from "axios"
-let temp = "50%";
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// const __dirname = dirname(fileURLToPath(import.meta.url));
-
-
-
-
-
-
- let imageArray = [];
-// // My apiKey
- const apiKey= "PdEFuffsLh2JVYqFYhwGQ17pPL3xUj6KGR3AiNKvPrz0KpwKqa2utVx8";
-
-
- const config = {
-     headers: { Authorization: apiKey },
-   };
-
-// // Creating express app and PORT
- const app = express();
- const port = 3000;
-// app.set('view engine', 'ejs');
-// app.use(express.static(__dirname + '/public'));
-
-// // The static styles in public
- app.use(express.static("public"))
-// app.use(express.json())
-app.use(bodyParser.urlencoded({extended : true}))
-
-
-
-
-
-
-
-
-  app.get("/", async (req,res)=>{
-      try{
-          const response = await axios.get("https://api.pexels.com/v1/curated?page=10&per_page=15", config);
-          const result = response.data;
-          // resArray.push(result.photos.src.original)
-          const randomIndex = Math.floor(Math.random() * result.photos.length);
-          const imageUrl = result.photos[randomIndex].src.landscape;
-          const userName = result.photos[randomIndex].photographer;
-          const userUrl = result.photos[randomIndex].photographer_url;
-          const randomly= Math.random() * result.photo;
-          console.log(randomly)
-          const randomImages = [];
-        for (let i = 0; i < result.photos.length; i++) {
-        const randomIndex = Math.floor(Math.random() * result.photos.length);
-        const imageUrl = result.photos[randomIndex].src.landscape;
-        randomImages.push(imageUrl);
-      }
-    console.log(randomImages)
-          console.log(randomImages)
-          res.render("home.ejs",{
-              imageUrl: imageUrl,
-              userName: userName,
-              userUrl: userUrl,
-              images: imageArray,
-              randomImages: randomImages,temp:temp
-          } )
-          console.log(temp)
-      }catch(err){
-          console.error(err)
-      }
-  })
-
-
-
-
-app.get("/search", async (req, res) => {
-     const { query } = req.query;
-     try {
-       const response = await axios.get(
-         `https://api.pexels.com/v1/search?query=${query}&page=10&per_page=15`,
-         config
-         )
-
-      const result = response.data;
-       imageArray = result.photos.map((photo) => ({
-        imageLink: photo.src.large,
-      }));
-      console.log(imageArray)
-      res.redirect("/")
-    } catch (err) {
-      console.error("Error fetching search results:", err);
-      res.status(500).json({ error: "Internal server error" });
-    }
-  });
-
-
-
-
-app.get("/index", (req,res)=>{
-  res.redirect("/")
-})
-
-
-
- // Serve static files like CSS and JS from a 'public' folder
-
-
-// // Listening server Port
- app.listen(port,()=>{
-     console.log("Server is working on PORT = ["+port+"]")
- })
-
-
+let temp="50%";let imageArray=[];const apiKey="PdEFuffsLh2JVYqFYhwGQ17pPL3xUj6KGR3AiNKvPrz0KpwKqa2utVx8";const config={headers:{Authorization:apiKey},};const app=express();const port=3000;app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:!0}))
+app.get("/",async(req,res)=>{try{const response=await axios.get("https://api.pexels.com/v1/curated?page=10&per_page=15",config);const result=response.data;const randomIndex=Math.floor(Math.random()*result.photos.length);const imageUrl=result.photos[randomIndex].src.landscape;const userName=result.photos[randomIndex].photographer;const userUrl=result.photos[randomIndex].photographer_url;const randomly=Math.random()*result.photo;console.log(randomly)
+const randomImages=[];for(let i=0;i<result.photos.length;i++){const randomIndex=Math.floor(Math.random()*result.photos.length);const imageUrl=result.photos[randomIndex].src.landscape;randomImages.push(imageUrl)}
+console.log(randomImages)
+console.log(randomImages)
+res.render("home.ejs",{imageUrl:imageUrl,userName:userName,userUrl:userUrl,images:imageArray,randomImages:randomImages,temp:temp})
+console.log(temp)}catch(err){console.error(err)}})
+app.get("/search",async(req,res)=>{const{query}=req.query;try{const response=await axios.get(`https://api.pexels.com/v1/search?query=${query}&page=10&per_page=15`,config)
+const result=response.data;imageArray=result.photos.map((photo)=>({imageLink:photo.src.large,}));console.log(imageArray)
+res.redirect("/")}catch(err){console.error("Error fetching search results:",err);res.status(500).json({error:"Internal server error"})}});app.get("/index",(req,res)=>{res.redirect("/")})
+app.get("/about",(req,res)=>{res.render("about.ejs")})
+app.get("/services",(req,res)=>{res.render("services.ejs")})
+app.listen(port,()=>{console.log("Server is working on PORT = ["+port+"]")})
--- public/config.js
+++ public/config.js
@@ -1,94 +1,13 @@
-let secondHeader = document.querySelector(".secondHeader");
-let apiKey = "383cb14d5884d28703457a69bdecabf6";
-let goUp = document.querySelector(".goUp");
-let dropNav = document.querySelector(".dropNav");
-console.log(dropNav)
-window.onscroll = ()=>{
-    if(window.scrollY >= 500){
-        secondHeader.style.top ="0";
-        secondHeader.style.opacity = "1";
-        secondHeader.style.display ="flex";
-        // console.log(window.scrollY)
-        goUp.style.display = "block";
-    }else{
-        secondHeader.top = "-60px";
-        secondHeader.style.opacity = "0";
-        secondHeader.style.display ="none";
-        goUp.style.display = "none";
-    }
-};
-
-
-const btn = document.querySelector(" .location");
-let updateImg= document.querySelector(" .updateImg");
-
-
-
- btn.addEventListener("click", getLocation)
- 
- function getLocation() {
-     if (navigator.geolocation) {
-         navigator.geolocation.getCurrentPosition(showPosition);
-     } else {
-         alert("Geolocation is not supported by this browser.");
-     }
- }
-  async function showPosition(position){
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-      console.log(latitude)
-      console.log(longitude)
-      // ABBREVIANDO LA API URL
-      let request = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
-      var res  =  await request.json();
-          if(res.weather[0].main == "Clear"){
-              updateImg.src = "images/sun.png";
-          }else if(res.weather[0].main == "Rain"){
-              updateImg.src = "images/heavy-rain.png";
-          }else if(res.weather[0].main == "Clouds"){
-              updateImg.src = "images/cloudy.png";
-          }else if(res.weather[0].main == "Snow"){
-              updateImg.src = "images/snow.png";
-          }else if(res.weather[0].main == "Thunderstorm"){
-              updateImg.src = "images/thunderstorm.png";
-          }else if(res.weather[0].main == "Haze"){
-              updateImg.src = "images/drizzle.png";
-          }
-      console.log(res)
-      }
-
-      let locationtwo = document.querySelector(".locationtwo");
-      locationtwo.addEventListener("click", getLoa_for_Btn_two)
-   
-       function getLoa_for_Btn_two(){
-        if (navigator.geolocation) { navigator.geolocation.getCurrentPosition(show_sec_btn_position);} else 
-        {alert("Geolocation is not supported by this browser.");
-        }
-      } async function show_sec_btn_position(position){
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        const updateImg_btn_two = document.querySelector(".updateImg_btn_two");
-        console.log(latitude)
-        console.log(longitude)
-        // ABBREVIANDO LA API URL
-        let request = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
-        var res  =  await request.json();
-            if(res.weather[0].main == "Clear"){
-                updateImg_btn_two.src = "images/sun.png";
-            }else if(res.weather[0].main == "Rain"){
-                updateImg_btn_two.src = "images/heavy-rain.png";
-            }else if(res.weather[0].main == "Clouds"){
-                updateImg_btn_two.src = "images/cloudy.png";
-            }else if(res.weather[0].main == "Snow"){
-                updateImg_btn_two.src = "images/snow.png";
-            }else if(res.weather[0].main == "Thunderstorm"){
-                updateImg_btn_two.src = "images/thunderstorm.png";
-            }else if(res.weather[0].main == "Haze"){
-                updateImg_btn_two.src = "images/drizzle.png";
-            }
-        console.log(res)
-      }
-
-    function showDrop(){
-        dropNav.classList.toggle("show")
-    }
+let secondHeader=document.querySelector(".secondHeader");let apiKey="383cb14d5884d28703457a69bdecabf6";let goUp=document.querySelector(".goUp");let dropNav=document.querySelector(".dropNav");console.log(dropNav)
+window.onscroll=()=>{if(window.scrollY>=500){secondHeader.style.top="0";secondHeader.style.opacity="1";secondHeader.style.display="flex";goUp.style.display="block"}else{secondHeader.top="-60px";secondHeader.style.opacity="0";secondHeader.style.display="none";goUp.style.display="none"}};const btn=document.querySelector(" .location");let updateImg=document.querySelector(" .updateImg");btn.addEventListener("click",getLocation)
+function getLocation(){if(navigator.geolocation){navigator.geolocation.getCurrentPosition(showPosition)}else{alert("Geolocation is not supported by this browser.")}}
+async function showPosition(position){const latitude=position.coords.latitude;const longitude=position.coords.longitude;console.log(latitude)
+console.log(longitude)
+let request=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);var res=await request.json();if(res.weather[0].main=="Clear"){updateImg.src="images/sun.png"}else if(res.weather[0].main=="Rain"){updateImg.src="images/heavy-rain.png"}else if(res.weather[0].main=="Clouds"){updateImg.src="images/cloudy.png"}else if(res.weather[0].main=="Snow"){updateImg.src="images/snow.png"}else if(res.weather[0].main=="Thunderstorm"){updateImg.src="images/thunderstorm.png"}else if(res.weather[0].main=="Haze"){updateImg.src="images/drizzle.png"}
+console.log(res)}
+let locationtwo=document.querySelector(".locationtwo");locationtwo.addEventListener("click",getLoa_for_Btn_two)
+function getLoa_for_Btn_two(){if(navigator.geolocation){navigator.geolocation.getCurrentPosition(show_sec_btn_position)}else{alert("Geolocation is not supported by this browser.")}}async function show_sec_btn_position(position){const latitude=position.coords.latitude;const longitude=position.coords.longitude;const updateImg_btn_two=document.querySelector(".updateImg_btn_two");console.log(latitude)
+console.log(longitude)
+let request=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);var res=await request.json();if(res.weather[0].main=="Clear"){updateImg_btn_two.src="images/sun.png"}else if(res.weather[0].main=="Rain"){updateImg_btn_two.src="images/heavy-rain.png"}else if(res.weather[0].main=="Clouds"){updateImg_btn_two.src="images/cloudy.png"}else if(res.weather[0].main=="Snow"){updateImg_btn_two.src="images/snow.png"}else if(res.weather[0].main=="Thunderstorm"){updateImg_btn_two.src="images/thunderstorm.png"}else if(res.weather[0].main=="Haze"){updateImg_btn_two.src="images/drizzle.png"}
+console.log(res)}
+function showDrop(){dropNav.classList.toggle("show")}
